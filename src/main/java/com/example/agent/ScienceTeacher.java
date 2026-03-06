88fba2d516192e827883ee4c9bd8c602b5f2bafb
hmendezve <125550871+hmendezve@users.noreply.github.com>
2026-03-06 14:02:25 +0100

    ADK Java AI Agents Hello World

---
+++ src/main/java/com/example/agent/ScienceTeacher.java
@@ -0,0 +1,22 @@
+// src/main/java/com/example/agent/ScienceTeacher.java
+package com.example.agent;
+
+import com.google.adk.agents.LlmAgent;
+import com.google.adk.web.AdkWebServer;
+
+public class ScienceTeacher {
+    public static void main(String[] args) {
+        //adkwebserver inicia un servidor web local
+        AdkWebServer.start(
+            LlmAgent.builder()
+                .name("science-teacher")
+                .description("A friendly science teacher")
+                .instruction("""
+                    You are a science teacher for teenagers.
+                    You explain science concepts in a simple, concise and direct way.
+                    """)
+                .model("gemini-2.5-flash")
+                .build()
+        );
+    }
+}
